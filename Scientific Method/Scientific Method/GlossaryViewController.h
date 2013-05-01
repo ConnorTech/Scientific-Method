047c9594b9c1487a64c2eef2c291eb31431ea98b
John Kotz <john.lyme@mac.com>
2013-05-01 09:11:42 -0400

    New veiw controllers

---
+++ Scientific Method/Scientific Method/GlossaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GlossaryViewController.h
+//  Scientific Method
+//
+//  Created by John Kotz on 5/1/13.
+//  Copyright (c) 2013 Lordtechy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GlossaryViewController : UIViewController
+
+@end
---
+++ Scientific Method/Scientific Method/ModelsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModelsViewController.h
+//  Scientific Method
+//
+//  Created by John Kotz on 5/1/13.
+//  Copyright (c) 2013 Lordtechy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelsViewController : UIViewController
+
+@end
